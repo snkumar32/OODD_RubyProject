c7ed12fdf354af7c203c7dd3ea19b885d580b32f
pgupta25@ncsu.edu <Nikku1996@>
2020-09-03 14:04:29 -0400

    login UI

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'access/index'
+  get 'access/login'
+  root 'welcome#homepage'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
