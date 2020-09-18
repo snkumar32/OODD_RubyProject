3cff8ef3b8f313b51c4b464caf400fff444fbd1b
amolgautam25 <amol_gautam25@yahoo.co.in>
2020-09-18 07:15:02 -0400

    added e-mail support

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,16 @@
 class UserMailer < ApplicationMailer
   def welcome_email
     @student = params[:student]
+    # @stud=Student.find(@student)
     @courses = CourseRegistration.find_by_sql("SELECT * from course_registrations where studentid=#{@student.id}")
-    mail(to: @student.email, subject: "You got a new order!")
+
+    @course_name_array = []
+
+    @courses.each do |i|
+      @course_name=Course.find_by_sql("SELECT name from courses where id=#{i.courseid}")
+      @course_name_array.append(@course_name)
+    end
+
+    mail(to: @student.email, subject: "IMP : Course Registration Update")
   end
 end
