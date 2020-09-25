4970cb13e4a3364e58c030c911729bdeba9ddc8e
snkumar32 <snkumar1912@gmail.com>
2020-09-24 23:11:04 -0400

    bug fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
     #check = User.select('category').find_by("email" => params[:email])
     #user = User.find_by(params[:email])
     #if user.category.eql?("Teacher")
+    $signup_email = current_user.email
     if current_user.category == "admin"
       "/admins/show"
     elsif current_user.category == "Teacher" || current_user.category == "Admin"
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 user = User.create!  :email => 'admin@admin.com', :password => 'admin123', :password_confirmation => 'admin123', :category => 'admin'
+
+"""
 s1 = User.create!  :email => 'skumar32@ncsu.edu', :password => 'sneha123', :password_confirmation => 'sneha123', :category => 'Student'
 s2 = User.create!  :email => 'pgupta25@ncsu.edu', :password => 'palash123', :password_confirmation => 'palash123', :category => 'Student'
 s2 = User.create!  :email => 'agautam6@ncsu.edu', :password => 'amol123', :password_confirmation => 'amol123', :category => 'Student'
@@ -21,7 +23,7 @@ c4 = Course.create! :name => 'Digital Circuit Design', :number => '410', :discip
 c5 = Course.create! :name => 'Digital Signal Processing', :number => '415', :discipline => 'ECE', :area => 'Signal Processing', :price => '130'
 c6 = Course.create! :name => 'Computer Networks', :number => '670', :discipline => 'CN', :area => 'Networks', :price => '100'
 c7 = Course.create! :name => 'Internet Protocol', :number => '671', :discipline => 'CN', :area => 'Networks', :price => '130'
-"""
+
 t1 = User.create!  :email => 'teacher1@ncsu.edu', :password => 'teacher1abc', :password_confirmation => 'teacher1abc', :category => 'Teacher'
 t2 = User.create!  :email => 'teacher2@ncsu.edu', :password => 'teacher2abc', :password_confirmation => 'teacher2abc', :category => 'Teacher'
 t3 = User.create!  :email => 'teacher3@ncsu.edu', :password => 'teacher3abc', :password_confirmation => 'teacher3abc', :category => 'Teacher'
